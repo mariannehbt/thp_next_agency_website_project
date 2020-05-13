27b8105d4c0bae803dcf36c5393e1fd920ae0bd4
mariannehbt <marianne.heribert@gmail.com>
2020-05-13 15:13:09 +0200

    add Navbar.jsx + set react-router

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch
+} from 'react-router-dom';
+import About from 'pages/About';
+import Home from 'pages/Home';
+import Navbar from 'components/Navbar';
+import Works from 'pages/Works';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const App = () => {
 	return (
-		<h1>Coucou !</h1>
+		<Router>
+			<Navbar />
+			<Switch>
+				<Route path='/about'>
+					<About />
+				</Route>
+				<Route exact path='/'>
+					<Home />
+				</Route>
+				<Route path='/works'>
+					<Works />
+				</Route>
+			</Switch>
+		</Router>
 	);
 };
 
---
+++ src/pages/Works.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Works = () => {
+	return (
+		<h1>Projets</h1>
+	);
+};
+
+export default Works;
